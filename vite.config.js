cd40198c4c673da89c2944f8e205af4a50dacdde
Gala P Segales <galapsegales@gmail.com>
2026-01-30 12:42:20 +0100

    upgrading new configuration for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/css-3-modern-art-gallery/', 
+  base: '/css-3-modern-art-gallery/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        location: resolve(__dirname, 'location.html')
+      }
+    }
+  }
 })
 
